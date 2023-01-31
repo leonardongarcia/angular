3a331d38c90a8c0a4d0662dd9f5ca8f759dde829
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2023-01-31 14:46:30 -0300

    Primeiro componente

---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-primeiro-componente',
+    template : "<h1>Primeiro componente</h1>",
+    styles: ['h1{color: red}']
+})
+export class AppPrimeiroComponente {
+
+}
